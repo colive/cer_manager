2da6a113d7ca120fe6d1210f3007cc8aab3a4986
colive <982446147@qq.com>
2014-03-12 00:48:12 +0800

    change forms.py

--- cer_manager/forms.py
+++ cer_manager/forms.py
@@ -1,16 +1,18 @@
 from django import newforms as forms
 
 class ContactForm(forms.Form):
-	name = forms.CharField(required=True)
-    证书路径 = forms.CharField(required=True)
-    Server = forms.CharField(required=True)
-    MD5 = forms.CharField(required=True)
-    生效时间 = forms.CharField(required=True)
-    过期时间 = forms.CharField(required=True)
-    负责人 = forms.CharField(required=True)
-    告警状态 = forms.CharField(required=True)
-    关注人 = forms.CharField(required=True)
-    模块ID = forms.IntegerField(required=True)
+    name = forms.CharField(required=True,label = "证书名称")
+    path = forms.CharField(required=True,label = "证书路径")
+    Server = forms.CharField(required=True,label = "Server")
+    md5 = forms.CharField(required=True,label = "MD5")
+    start_date = forms.CharField(required=True,label = "生效时间")
+    end_date = forms.CharField(required=True,label = "过期时间")
+    server_dev = forms.CharField(required=True,label = "负责人")
+    alarm_status = forms.CharField(required=True,label = "告警状态")
+    attention = forms.CharField(required=True,label = "关注人")
+    server_id = forms.IntegerField(required=True,label = "模块ID")
+    '''
     def clean_info(self):
         name = self.cleaned_data['证书名称']	
         return message
+        '''
