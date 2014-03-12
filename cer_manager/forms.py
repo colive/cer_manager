558ed898b43c69226af03accf69c75d6f769f635
colive <982446147@qq.com>
2014-03-12 23:43:41 +0800

    change insert.html work

--- cer_manager/forms.py
+++ cer_manager/forms.py
@@ -1,16 +1,17 @@
-from django import newforms as forms
+#-*-coding: utf-8 -*-
+from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(required=True,label = "证书名称")
-    path = forms.CharField(required=True,label = "证书路径")
-    Server = forms.CharField(required=True,label = "Server")
+    name = forms.CharField(required=True,label = u'证书名称')
+    path = forms.CharField(required=True,label = u'证书路径')
+    Server = forms.CharField(required=True,label = u'Server')
     md5 = forms.CharField(required=True,label = "MD5")
-    start_date = forms.CharField(required=True,label = "生效时间")
-    end_date = forms.CharField(required=True,label = "过期时间")
-    server_dev = forms.CharField(required=True,label = "负责人")
-    alarm_status = forms.CharField(required=True,label = "告警状态")
-    attention = forms.CharField(required=True,label = "关注人")
-    server_id = forms.IntegerField(required=True,label = "模块ID")
+    start_date = forms.CharField(required=True,label = u'生效时间')
+    end_date = forms.CharField(required=True,label = u'过期时间')
+    server_dev = forms.CharField(required=True,label = u'负责人')
+    alarm_status = forms.CharField(required=True,label = u'告警状态')
+    attention = forms.CharField(required=True,label = u"关注人")
+    server_id = forms.IntegerField(required=True,label = u'模块ID')
     '''
     def clean_info(self):
         name = self.cleaned_data['证书名称']	
--- cer_manager/views.py
+++ cer_manager/views.py
@@ -3,15 +3,13 @@ from  django.shortcuts  import  render_to_response
 from django.http import HttpResponse
 from cer_manager.cer_manager.models import DbCertificate 
 import MySQLdb
+from forms import ContactForm
 
 def cer_list(request):
 	posts = DbCertificate.objects.all()
 	t = loader.get_template("index.html")
 	c = Context({'posts':posts})
 	return HttpResponse(t.render(c))
-#def insert(request,param1):
-#	t = loader.get_template("canshu.html")
-#	c = Context({'canshu':param1})
 	return HttpResponse(t.render(c))
 def ip_insert(request):
     return render_to_response('ip_insert.html')
@@ -25,9 +23,9 @@ def modify(request,id):
 '''
 def insert(request):
     if request.method == 'POST':
-        form = ContactForm(request.POST)  #实例化
+        form = ContactForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_data   #清洗数据，转化为python通用的Unicode类型
+            cd = form.cleaned_data   
             )
             return HttpResponseRedirect('error.html')
     else:
@@ -35,12 +33,14 @@ def insert(request):
     return render_to_response('insert.html', {'form': form})
 '''
 def insert(request):
-	if request.method == 'POST':
-		form = ContactForm(request.POST)
-		if form.is_valid():
-			cer_info = form.cleaned_data
-	else:
-		form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+	if form.is_valid():
+		cer_info = form.cleaned_data
+        name = cer_info['name']
+         
+    else:
+        form = ContactForm()
     return render_to_response('insert.html', {'form': form})
 
     
